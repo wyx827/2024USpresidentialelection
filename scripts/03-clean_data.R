4bc811880c0d0a589b3a445313a3a7cdd2e58583
“Casseliu77 <cassieliuliu1022@gmail.com>
2024-10-14 21:03:39 -0400

    Update the cleaned data and script

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -15,27 +15,28 @@ colnames(raw_data)
 
 #### Clean the poll data
 # Filter for likely voters (lv)
-likely_voters_data <- raw_data %>% filter(population == "lv")
+#likely_voters_data <- raw_data %>% filter(population == "lv")
 
 # Sort pollsters based on numeric_grade and pollscore
-reliable_pollsters <- likely_voters_data %>%
-  filter(!is.na(numeric_grade) & !is.na(pollscore)) %>%
-  arrange(desc(numeric_grade), desc(pollscore))
+#reliable_pollsters <- likely_voters_data %>%
+  #filter(!is.na(numeric_grade) & !is.na(pollscore)) %>%
+  #arrange(desc(numeric_grade), desc(pollscore))
 
 # Rank pollsters by transparency_score as well
 #Higher transparency scores indicate more detailed methodology information
-reliable_pollsters <- reliable_pollsters %>%
-  filter(!is.na(transparency_score)) %>%
-  arrange(desc(transparency_score))
+#reliable_pollsters <- reliable_pollsters %>%
+#  filter(!is.na(transparency_score)) %>%
+ # arrange(desc(transparency_score))
 
 # Consider methodology
 # Extract the columns that provide relevant information
-final_pollster_ranking <- reliable_pollsters %>%
-  dplyr::select(pollster, numeric_grade, pollscore, transparency_score, methodology)
+#final_pollster_ranking <- reliable_pollsters %>%
+ # dplyr::select(pollster, numeric_grade, pollscore, transparency_score, methodology)
 
 
-# Now we decide to use Marquette Law School with highest numeric grade and transparency
-cleaned_data <- raw_data %>% filter(pollster=="Marquette Law School") 
+
+# Now we decide to use pollster with grade >2.5 as 'high quality' pollster
+cleaned_data <- raw_data %>% filter(numeric_grade > 2.5)
 # Clean raw dataset, remove columns with many missing values and keep the columns we want to make analysis on
 cleaned_data <- cleaned_data %>% dplyr::select(pollster, methodology, numeric_grade, start_date,
                                                end_date, sample_size, population, state, 
