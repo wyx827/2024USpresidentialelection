4b60c49d9a9a772329383985062bcb9ad47769f4
“Casseliu77 <cassieliuliu1022@gmail.com>
2024-10-21 18:16:07 -0400

    Delete useless code.

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -14,21 +14,15 @@ raw_data <- read_csv("data/01-raw_data/US_voting.csv")
 colnames(raw_data)
 
 #### Clean the poll data
-# Filter for likely voters (lv)
-#likely_voters_data <- raw_data %>% filter(population == "lv")
-
 # Now we decide to use pollster with grade >2.5 as 'high quality' pollster
 cleaned_data <- raw_data %>% filter(numeric_grade > 2.5)
 cleaned_data$mdy <- mdy(cleaned_data$end_date)
-cleaned_data <- cleaned_data %>%
-  filter(candidate_name== "Donald Trump")
+cleaned_data <- cleaned_data %>% filter(candidate_name== "Donald Trump")
 
 # Clean raw dataset, remove columns with many missing values and keep the columns we want to make analysis on
 cleaned_data <- cleaned_data %>% dplyr::select(pollster_rating_name, methodology, numeric_grade, start_date,
                                                end_date, sample_size, population, state, 
-                                               candidate_name, pct)
-
-#filter(!is.na(population) & !is.na(state) & !is.na(candidate_name) & !is.na(pct) & !is.na(numeric_grade& )
+                                           candidate_name, pct) 
 
 # Change name for different types of voters in population column
 cleaned_data <- cleaned_data %>%
@@ -45,19 +39,18 @@ cleaned_data <- cleaned_data %>% select(-population)
 cleaned_data <- cleaned_data %>%
   filter(end_date >= start_date)
 
-# convert the format of the start date and end date
+# Convert the format of the start date and end date
 cleaned_data <- cleaned_data %>%
   mutate(
-    start_date = mdy(start_date),  # or dmy(), ymd() depending on the format
-    end_date = mdy(end_date)       # same as above
-  )
+    start_date = mdy(start_date), 
+    end_date = mdy(end_date))
 
 # Revise the state name
 cleaned_data <- cleaned_data %>%
   mutate(state = ifelse(is.na(state), "National",
                         ifelse(state == "Nebraska CD-2", "Nebraska",
                                ifelse(state %in% c("Maine CD-1", "Maine CD-2"), "Maine", state))))
-# revise the name of column 
+# Revise the name of column 
 cleaned_data <- cleaned_data %>%
   rename(percent = pct)
 
