192e1ab7a225f429e3723b027d4c71d3e75ce3bb
“Casseliu77 <cassieliuliu1022@gmail.com>
2024-10-21 23:55:27 -0400

    Update the EDA about starter model part.

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -12,107 +12,99 @@ library(tidyverse)
 #install.packages("rstanarm")
 library(rstanarm)
 library(dplyr)
+library(ggplot2)
+library(modelsummary)
 
 #### Read data ####
 analysis_data <- read_csv("data/02-analysis_data/cleaned_US_voting.csv")
 colnames(analysis_data)
 
 #### Summary the numeric variables ####
-summary(cleaned_data)
+summary(analysis_data)
 summary(analysis_data$sample_size)
-summary(analysis_data$pct)
+summary(analysis_data$percent)
 summary(analysis_data$numeric_grade)
 
-# Histogram of support percentages (pct) for Trump
-ggplot(cleaned_data, aes(x = percent)) +
+### See the distribution for some useful variables ###
+# Histogram of support percentages for Trump
+ggplot(analysis_data, aes(x = percent)) +
   geom_histogram(binwidth = 2, fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Distribution of Support (pct) for Donald Trump", x = "Support Percentage (pct)", y = "Count") +
+  labs(title = "Distribution of Support (percent) for Donald Trump", x = "Support Percentage (percent)", y = "Count") +
   theme_minimal()
 
 # Histogram of sample size to see its distribution
-ggplot(cleaned_data, aes(x = sample_size)) +
+ggplot(analysis_data, aes(x = sample_size)) +
   geom_histogram(binwidth = 100, fill = "green", color = "black", alpha = 0.7) +
   labs(title = "Distribution of Sample Size", x = "Sample Size", y = "Count") +
   theme_minimal()
 
-# Line plot of candidate support over time
-ggplot(candidates, aes(x = as.Date(start_date, format = "%m/%d/%y"), y = pct, color = candidate_name)) +
+# Line plot of Trump's support over time by end data
+ggplot(analysis_data, aes(x = as.Date(end_date, format = "%y/%m/%d"), y = percent, color = candidate_name)) +
   geom_line() +
-  labs(title = "Candidate Support Over Time", x = "Date", y = "Support Percentage (pct)") +
+  labs(title = "Candidate Support Over Time", x = "Date", y = "Support Percentage (percent)") +
   theme_minimal()
 
-# Boxplot to compare support percentages across candidates
-ggplot(candidates, aes(x = candidate_name, y = pct, fill = candidate_name)) +
-  geom_boxplot() +
-  labs(title = "Comparison of Support Across Candidates", x = "Candidate", y = "Support Percentage (pct)", fill="Candidate Name") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-# View the updated dataset
-head(cleaned_data)
-
-# Bar plot of candidate support by population type
-ggplot(cleaned_data, aes(x = candidate_name, y = percent, fill = population_group)) +
+# Bar plot of Trump support by population type
+ggplot(analysis_data, aes(x = candidate_name, y = percent, fill = population_group)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Candidate Support by Population Type", x = "Candidate", y = "Support Percentage (pct)") +
+  labs(title = "Candidate Support by Population Type", x = "Candidate", y = "Support Percentage (percent)",
+       fill = "Population Group") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-candidates |>
-  filter(pct > 1) |>
-  ggplot(aes(x = sample_size, y = pct, 
-             color = candidate_name)) +
+
+### Relationship between Variables ###
+# Scatter plot of relationship between gained percent and pollster's grade
+analysis_data |>
+  ggplot(aes(x = percent, y = numeric_grade)) +
+  geom_point() +  
+  facet_wrap(vars(pollster_rating_name)) + 
+  labs(y = "Numeric Grade", x = "Percent") +
+  theme_minimal()
+
+# Scatterplot about size and percentage by methodology
+analysis_data |>
+  filter(percent > 1) |>
+  ggplot(aes(x = sample_size, y = percent, 
+             color = candidate_name)) + facet_wrap(vars(methodology)) + theme_minimal() +
   geom_point(size = 1, alpha = 0.2) +
   theme_classic() +
   labs(
     x = "Sample Size",
-    y = "Percentage",
-    color = "Candidate Name"
-  ) +
-  scale_color_brewer(palette = "Set1")
+    y = "Percentage", color = "Candidate Name") + scale_color_brewer(palette = "Set1")
+
+# Stacked Bar Chart of Percent by Numeric Grade and Population Group
+analysis_data |>
+  filter(numeric_grade > 2.5) |>
+  ggplot(aes(x = numeric_grade, y = percent, fill = population_group)) +
+  geom_bar(stat = "identity") + 
+  theme_classic() +
+  labs(
+    x = "Numeric Grade",
+    y = "Percent",
+    title = "Stacked Bar Chart of Percent by Numeric Grade and Population Group",
+    fill = "Population Group")
 
 
-#### Model data #####
+#### Model Data and Construct Some Basic Starter Models #####
+colnames(analysis_data)
 
 ## Starter models ##
-# Model 1: pct as a function of end_date for Trump
-model_trump_date <- lm(percent ~ end_date, data = cleaned_data)
+#1. Logistic regression model: regarding to the state, grade and methodology influence on the gained percent
+logistic_reg <- glm(percent/100 ~
+        state + numeric_grade + methodology, data = analysis_data, family = quasibinomial)
 
-# Model 2: pct as a function of end_date and pollster for Trump
-model_trump_date_pollster <- lm(percent ~ end_date + pollster_rating_name, data = cleaned_data)
+# Summary the logistic regression model
+modelsummary(logistic_reg)
 
-# Augment the data with model predictions
-just_trump_high_quality <- cleaned_data |>
-  mutate(
-    fitted_trump_date = predict(model_trump_date),
-    fitted_trump_date_pollster = predict(model_trump_date_pollster)
-  )
-
-# Model 1: Plot for Trump based on end_date
-ggplot(just_trump_high_quality, aes(x = end_date)) +
-  geom_point(aes(y = percent), color = "black", size = 1) +
-  geom_line(aes(y = fitted_trump_date), color = "blue", linetype = "dotted") +
-  theme_classic() +
-  labs(y = "Trump Percent", x = "Date", title = "Linear Model 1:Trump Poll Percent vs. Date")
-
-# Model 2: Plot for Trump based on end_date and pollster
-ggplot(just_trump_high_quality, aes(x = end_date)) +
-  geom_point(aes(y = percent), color = "black", size = 0.5) +
-  geom_line(aes(y = fitted_trump_date_pollster), color = "blue", linetype = "dotted") +
-  facet_wrap(vars(pollster_rating_name)) +
-  theme_classic() +
-  labs(y = "Trump Percent", x = "Date", title = "Linear Model 2: Trump Poll Percent vs. Date by Pollster")
-
-## Bayesian Model ##
-just_trump_high_quality2 <- cleaned_data |>
-  mutate(
-    pollster = factor(pollster_rating_name),    # Convert pollster to a factor
-    state = factor(state),          # Convert state to a factor
-    num_trump = round((percent / 100) * sample_size, 0)
-  )
+#2. Bayesian Model Initial Build up
+bayesian_model <- analysis_data |>
+  mutate(population = factor(population_group),   
+    state = factor(state),         
+    num_trump = round((percent / 100) * sample_size, 0))
 
 # Model formula
-model_formula_1 <- cbind(num_trump, sample_size - num_trump) ~ (1 | pollster)
+model_formula_1 <- cbind(num_trump, sample_size - num_trump) ~ (1 | population)
 
 # Specify priors
 priors <- normal(0, 2.5, autoscale = TRUE)
@@ -120,14 +112,13 @@ priors <- normal(0, 2.5, autoscale = TRUE)
 # Fit Bayesian model using stan_glmer
 bayesian_model_1 <- stan_glmer(
   formula = model_formula_1,
-  data = just_trump_high_quality2,
-  family = binomial(link = "logit"),  # Use binomial family for logistic regression
+  data = bayesian_model,
+  family = binomial(link = "logit"),
   prior = priors,
   prior_intercept = priors,
   seed = 123,
   cores = 4,
-  adapt_delta = 0.95
-)
+  adapt_delta = 0.95)
 
 # Posterior predictive checks
 pp_check(bayesian_model_1)
@@ -136,19 +127,21 @@ pp_check(bayesian_model_1)
 summary(bayesian_model_1)
 
 # Model formula with state as an additional random effect
-model_formula_2 <- cbind(num_trump, sample_size - num_trump) ~ (1 | pollster) + (1 | state)
+model_formula_2 <- cbind(num_trump, sample_size - num_trump) ~ (1 | population) + (1 | state)
+
+# Specify priors
+priors <- normal(0, 2.5, autoscale = TRUE)
 
 # Fit Bayesian model using stan_glmer
 bayesian_model_2 <- stan_glmer(
   formula = model_formula_2,
-  data = just_trump_high_quality2,
-  family = binomial(link = "logit"),  # Use binomial family for logistic regression
+  data = bayesian_model2,
+  family = binomial(link = "logit"),
   prior = priors,
   prior_intercept = priors,
   seed = 123,
   cores = 4,
-  adapt_delta = 0.95
-)
+  adapt_delta = 0.95)
 
 # Posterior predictive checks
 pp_check(bayesian_model_2)
@@ -157,16 +150,26 @@ pp_check(bayesian_model_2)
 summary(bayesian_model_2)
 
 # Plot random effects for pollster
-plot(bayesian_model_2, pars = "(Intercept)", prob = 0.95) + ggtitle("Bayesian Model 1: Pollster Effect") + theme(plot.title = element_text(hjust = 0.5))
+plot(bayesian_model_1, pars = "(Intercept)", prob = 0.95) + 
+  ggtitle("Bayesian Model 1: Population Group Effect") + theme(plot.title = element_text(hjust = 0.5))
 
 # Plot random effects for pollster and state
-plot(bayesian_model_2, pars = "(Intercept)", prob = 0.95) + ggtitle("Bayesian Model 2: Pollster Effect and State Effects") + theme(plot.title = element_text(hjust = 0.5))
-
+plot(bayesian_model_2, pars = "(Intercept)", prob = 0.95) + ggtitle("Bayesian Model 2: Popultion Effect and State Effects") 
++ theme(plot.title = element_text(hjust = 0.5))
 
 
 #### Save model ####
-#saveRDS(
-#  just_trump_high_quality,
-#  file = "models/first_model_1.rds")
+#saveRDS(logstic_reg, file = "models/logistic_reg.rds")
+#saveRDS(bayesian_model_1, file = "models/bayesian_model_1.rds")
+#saveRDS(bayesian_model_2, file = "models/bayesian_model_2.rds")
+
+#### Completion Message ####
+print("EDA has completed.")
+
+
+
+
+
+
 
 
